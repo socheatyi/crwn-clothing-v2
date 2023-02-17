6a3d13c62d8a528ff2751b942d92469c56022537
Socheat Yi <socheatyi@Socheats-MacBook-Pro.local>
2023-02-16 19:50:55 -0800

    added reselect for memoization to preview reload select when same data reload when switch component

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,4 +1,29 @@
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('selector fired');
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+
+  }
+);
+
+/*
 export const selectCategoriesMap = (state) => {
+    console.log('selector fired');
+
     const categoriesMap = state.categories.categories.reduce(
       (acc, { title, items }) => {
         acc[title.toLowerCase()] = items;
@@ -7,4 +32,4 @@ export const selectCategoriesMap = (state) => {
       {}
     );
     return categoriesMap;
-  };
+  };*/
