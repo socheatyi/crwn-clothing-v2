91ae8ddfe80091d2bac24d52331123f73b3fb03f
Socheat Yi <socheatyi@Socheats-MacBook-Pro.local>
2023-02-17 21:58:19 -0800

    replace redux-thunk with redux-saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,14 +1,6 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
 
-/* Before using reduct-thunk
-export const setCategories = (categories) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
-*/
-
-// Move getCategoriesAndDocuments to here from component
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
 export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 
@@ -20,15 +12,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 
 export const fetchCategoriesFailure = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesStartAsync = () => {
-  return async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-    try {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-      dispatch(fetchCategoriesFailure(error));
-    }
-  };
-};
