d4849136094cf315300d504bfa413f0546877dd5
Socheat Yi <socheatyi@Socheats-MacBook-Pro.local>
2023-02-17 20:04:45 -0800

    cart reduct store and redux-persist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
@@ -14,11 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <CartProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <App />
-        </CartProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,40 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import logger from 'redux-logger';
 
-import { rootReducer } from './root-redcucer';
+import { rootReducer } from './root-reducer';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-    Boolean
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('next state: ', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares)); // Crate multiple middlewares from left to right
-
-export const store = createStore(rootReducer, undefined, composedEnhancers); // Middle param is additional state
+export const persistor = persistStore(store);
